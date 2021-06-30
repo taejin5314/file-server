ae75eca55a76f3ff6d4a18ffea2a4c28991ecac4
Taejin Jung <taejin5314@gmail.com>
2021-06-30 16:33:00 +0000

    succeeded to transmit html and txt file

--- client.js
+++ client.js
@@ -1,6 +1,7 @@
 const { IP, PORT } = require('./constants');
 const net = require('net');
 const readline = require('readline');
+const fs = require('fs');
 
 const rl = readline.createInterface({
   input: process.stdin,
@@ -12,20 +13,55 @@ const conn = net.createConnection({
   port: PORT // PORT constant
 });
 
-const recursiveReadLine = function() {
+let fileExist = false, copySuccess = false;
+const fileReadLine = function() {
   // ask client about the file that they are looking for.
   rl.question('Enter the file name you are looking for(type exit to end): ', (answer) => {
-    if (answer === 'exit') return rl.close();
-    else {
-      conn.write(answer);
-      conn.on('data', (data) => console.log(data));
-      recursiveReadLine();
+    if (answer === 'exit') {
+      conn.end();
+      return rl.close();
     }
+    conn.write(answer);
+    conn.on('data', (data) => {
+      if (!fileExist && data === `There is ${answer} in the server!`) { // if there is a file that client wants
+        fileExist = true;
+        console.log(data);
+        rl.question('Do you want to copy this in your device?(Y/N) ', (answer2) => {
+          conn.write(answer2);
+          // if the client enter n or N, end the program!
+          if (answer2 === 'n' || answer2 === 'N') {
+            conn.end();
+            return rl.close();
+          }
+        });
+      }
+      if (fileExist) {
+        if (data.slice(0, 12) === 'copying file') {
+          // console.log(data.slice(13));
+          fs.writeFile(`./client-files/${answer.split('.')[0]}_downloaded.${answer.split('.')[1]}`, data.slice(13), function(err) {
+            if (err) {
+              return console.log(err);
+            }
+            copySuccess = true;
+            if (copySuccess) {
+              console.log(`Saved ${answer} in "./client-files/${answer.split('.')[0]}_downloaded.${answer.split('.')[1]}".`);
+              copySuccess = false;
+              fileExist = false;
+              conn.end();
+              return rl.close();
+            }
+          });
+        }
+      }
+      
+    });
+    
   });
 };
 
 conn.setEncoding('utf8'); // interpret data as text
+// when client is connected to the server
 conn.on('connect', () => {
   console.log('Connected to the server!');
-  recursiveReadLine();
+  fileReadLine();
 });
--- server.js
+++ server.js
@@ -8,13 +8,32 @@ server.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}!`);
 });
 
+// when the client is connected to the server
+let filePath, fileExist = false;
 server.on('connection', (client) => {
   console.log('New client connected!');
   client.setEncoding('utf8');
   client.on('data', (data) => {
     console.log('From client:', data);
-    if (stats(`./server-files/${data}`).isFile()) {
-      client.write(`There is ${data}!`);
+    if (!fileExist) {
+      if (stats(`./server-files/${data}`).isFile()) {
+        filePath = `./server-files/${data}`;
+        fileExist = true;
+        client.write(`There is ${data} in the server!`);
+      }
+    } else if (fileExist && (data === 'Y' || data === 'y')) {
+      fs.readFile(filePath, 'utf8', function(err, contents) {
+        if (err) {
+          console.log(err);
+        } else {
+          client.write('copying file\n' + contents);
+          fileExist = false;
+        }
+      });
+    } else {
+      client.write("Copy cancelled!");
+      client.end();
+      fileExist = false;
     }
   });
 });
